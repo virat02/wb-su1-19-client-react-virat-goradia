23c57739efeb8ee6d1feabe68b955a3a09cbf03c
virat02 <virat.goradia96@gmail.com>
2019-06-01 10:42:46 -0400

    Implemented List widget

---
+++ src/components/ListWidget.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import "../css/WidgetCss.css";
+import WidgetHeaderComponent from "./WidgetHeaderComponent";
+
+export default class ListWidget extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            listOrderType: "ul",
+            listTextDisplay: this.props.widget.text,
+            listText : (this.props.widget.text).split('\n')
+        }
+    }
+
+    changeListOrder = event=>
+        this.setState( {
+            listOrderType: event.target.value
+        });
+
+    listTextChanged = event =>
+        this.setState( {
+            listTextDisplay: event.target.value,
+            listText : (event.target.value).split('\n')
+        });
+
+    render() {
+
+        console.log(this.state.listText);
+
+        return (
+            <div>
+                <WidgetHeaderComponent
+                    widget={this.props.widget}
+                    deleteWidget = {this.props.deleteWidget}/>
+                <br />
+                <textarea className="form-control form-control-lg col-lg-12 float-left widgetTextbox"
+                          rows="4" cols="50"
+                          value={this.state.listTextDisplay}
+                          onChange={this.listTextChanged}>
+                </textarea>
+
+                <select
+                    className="form-control form-control-lg col-lg-12 float-left widgetTextbox"
+                    onChange = {this.changeListOrder}
+                    value={this.state.listOrderType}>
+                    <option value="ul">Unordered list</option>
+                    <option value="ol">Ordered list</option>
+                </select>
+
+                <input className="form-control form-control-lg col-lg-12 float-left widgetTextbox"
+                       height="10px" placeholder="Widget Name"/>
+
+                <h3> Preview </h3>
+
+                <div>
+                    {
+                        this.state.listOrderType === "ul" ?
+                            <ul> {this.state.listText.map( (line,index) =>
+                                <li key={index}>{line}</li>)} </ul>
+                            :
+                            <ol> {this.state.listText.map( (line,index) =>
+                                <li key={index}>{line}</li>)} </ol>
+                    }
+                </div>
+
+            </div>
+        );
+    }
+}
--- src/components/ParagraphWidget.js
+++ src/components/ParagraphWidget.js
@@ -6,7 +6,16 @@ export default class ParagraphWidget extends React.Component {
 
     constructor(props) {
         super(props);
+
+        this.state = {
+            paragraphText : this.props.widget.text
+        }
     }
+
+    paragraphTextChanged = event =>
+        this.setState( {
+            paragraphText : event.target.value
+        });
 
     render() {
         return (
@@ -17,13 +26,18 @@ export default class ParagraphWidget extends React.Component {
                 <br />
                 <textarea className="form-control form-control-lg col-lg-12 float-left widgetTextbox"
                           rows="4" cols="50"
-                          defaultValue={this.props.widget.text}>
+                          value={this.state.paragraphText}
+                          onChange={this.paragraphTextChanged}>
                 </textarea>
 
                 <input className="form-control form-control-lg col-lg-12 float-left widgetTextbox"
                        height="10px" placeholder="Widget Name"/>
 
                  <h3> Preview </h3>
+
+                <div>
+                    <p> {this.state.paragraphText} </p>
+                </div>
 
             </div>
         );
